783f7e02fcfb8eafefbe1dbb49147604353470a5
Кей Альтос <igor170491@yandex.ru>
2022-10-28 09:55:03 +0300

    Added base game cycle

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print('I hate Pycharm and Python!')
-print('C# forever!')
+import pygame
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+SCALE = 100
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Hotline Miami')
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(WHITE)
+        pygame.draw.rect(screen, BLACK, (100, 100, SCALE, 2*SCALE))
+        pygame.display.flip()
+        clock.tick(FPS)
+
+main()
